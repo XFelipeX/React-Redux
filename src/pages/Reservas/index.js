0b7a679c253de779faf3146df653793f0f554fdf
Felipe Dias <felipediasamorimpessoa@gmail.com>
2020-10-29 16:20:35 -0300

    correcao reserva

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,38 +1,43 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { MdDelete } from 'react-icons/md';
 import './style.css';
-import {MdDelete} from 'react-icons/md';
-
-import {useSelector} from 'react-redux';
 
 export default function Reservas() {
+ const dispatch = useDispatch();
+ const reserves = useSelector(state => state.reserve);
 
-  const reserves = useSelector(state => state.reserve);
-
+ function handleRemove(id){
+  dispatch({
+    type: 'REMOVE_RESERVE',
+    id,
+  })
+ }
 
  return (
    <div>
-    <h1>Voce solicitou {reserves.length} reservas</h1>
-    
-    {reserves.map(reserve =>(
+    <h1 className="title">Voce solicitou {reserves.length} reservas</h1>
+
+    {reserves.map(reserve => (
       <div className="reservas" key={reserve.id}>
-      <img src={reserve.image} alt={reserve.title}/>
-
-      <strong>{reserve.title}</strong>
-      <span>Quantidade: {reserve.amount}</span>
-
-      <button type="button" onClick={() => {}}>
-        
-        <MdDelete size={20} color="#fff" />
+        <img
+        src={reserve.image}
+        alt={reserve.title}
+        />
+        <strong>{reserve.title}</strong>
+        <span>Quantidade: {reserve.amount}</span>
+        <button
+        type="button"
+        onClick={()=> handleRemove(reserve.id) }
+        >
+        <MdDelete size={20} color="#191919" />
       </button>
     </div>
-    ))};
-
-    
+    ))}
 
     <footer>
-
-     <button type="button">Solicitar Reservas</button>
-   </footer>
+      <button type="button">Solicitar Reservas</button>
+    </footer>
 
    </div>
  );
